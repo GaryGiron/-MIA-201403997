543f0bd463515c4e1896c82a5ff1cbf765cbd898
GaryGiron <gsteph393@gmail.com>
2016-07-31 00:28:49 -0600

    Commit cambios estructura

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+
+int main(void){
+ char cadena [100];
+
+ int a=0;
+ while(a==0){
+  printf ("File System ext2/ext3:#~$ ");
+  fgets (cadena, 100, stdin);
+  int len = strlen(cadena);
+  int i;
+
+  char str[]="/storage/SD:/storage/USB1";
+    const char* del=":";
+    char* arr[2];
+    split(arr, str, del);
     return 0;
+ }
+
+
+return 0;
 }
+
+void split(char** dest, char* src, const char* del){
+    char* token = strtok(src, del);
+    while(token!=NULL){
+       *dest++ = token;
+       token = strtok(NULL, del);
+    }
+}
+
